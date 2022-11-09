801cd07255ee22fcded5fd376e8881ca5dfb525f
mfgulmez <mfgulmez2001@gmail.com>
2022-11-09 21:23:21 +0300

    Add project files.

---
+++ E-Commerce Web Application/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce_Web_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+        public ActionResult shop() {
+            return View();
+
+        }
+    }
+}
